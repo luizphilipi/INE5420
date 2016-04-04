db4efe045cd4af9773fafa0513ab776eb34193f4
luizphilipi <luizphilipi.m@gmail.com>
2016-04-03 23:11:38 -0300

    Adicionada pop-up para indicar que não há objetos selecionados

--- src/TelaPrincipal.cpp
+++ src/TelaPrincipal.cpp
@@ -417,6 +417,12 @@ char* TelaPrincipal::getObjetoSelecionado() {
 		return name;
 	}
 
+	GtkDialogFlags flags = GTK_DIALOG_DESTROY_WITH_PARENT;
+	GtkWidget *dialog = gtk_message_dialog_new(GTK_WINDOW(gtk_builder_get_object( builder, TELA_PRINCIPAL )), flags, GTK_MESSAGE_ERROR,
+			GTK_BUTTONS_CLOSE, "Selecione um objeto");
+	gtk_dialog_run(GTK_DIALOG(dialog));
+	gtk_widget_destroy (dialog);
+
 	return NULL;
 }
 
